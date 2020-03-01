95b23044370c19c5194a15aa3d751d06ce96c028
helena <helenawang77@gmail.com>
2020-03-01 13:56:11 -0800

    updated 1.3

--- CrackingTheCodeInterview/src/Chapter1_3/URLify.java
+++ CrackingTheCodeInterview/src/Chapter1_3/URLify.java
@@ -3,8 +3,48 @@ package Chapter1_3;
 public class URLify {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		String s = "My lover is A   ";
+		int n = 13;
+		String s2 = "Mr John Smith        ";
+		
+		char[] arr = s2.toCharArray();
+		// Use self algo
+		System.out.println("\"" + String.valueOf(URLify(arr, n)) + "\"");
+		
+		// Use library
+		String s2new = s2.substring(0, n);
+		System.out.println("\"" + s2new.replace(" ", "%20") + "\"");
 
+	}
+	
+	public static char[] URLify(char[] arr, int n) {
+		int count = 0;
+		int spaceIndex = n;
+		int indexOri = n - 1;
+		
+		// count space
+		for (int i = 0; i < arr.length; i++) {
+			if (i < n && arr[i] == ' ')
+				count += 2;
+			if (i >= n)
+				arr[i] = '\0';
+		}	
+		
+		
+		// replace " " to "20%"
+		for (int i = n + count - 1; i >= 0; i--) {
+			if (arr[indexOri] == ' ') {
+				arr[i] = '0';
+				arr[i - 1] = '2';
+				arr[i - 2] = '%';
+				i -= 2;
+			} else 
+				arr[i] = arr[indexOri];
+			indexOri--;
+		}
+		
+		
+		return arr;		
 	}
 
 }
